ab45fba5c25e633503a7d96dd946f8e72743ea56
copipastaguy <narutoalf@yahoo.com>
2022-01-27 23:43:19 +0800

    file management

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import Header from "./components/Header";
-import SideBar from "./components/SideBar";
-import RecommendedVideos from "./components/RecommendedVideos";
-import SearchPage from "./components/SearchPage";
+import Header from "./components/Header/Header.js";
+import SideBar from "./components/SideBar/SideBar.js";
+import RecommendedVideos from "./components/RecommendedVideos/RecommendedVideos.js";
+import SearchPage from "./components/SearchPage/SearchPage.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <div className="app">
       <Router>
         {/* Put component outside of switch for it to be displayed permenantly */}
-      <Header />
+        <Header />
 
         <Switch>
           <Route path="/search/:searchTerm">
@@ -26,7 +26,6 @@ function App() {
               <RecommendedVideos />
             </div>
           </Route>
-
         </Switch>
       </Router>
     </div>
